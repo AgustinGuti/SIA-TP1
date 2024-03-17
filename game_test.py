39bcff5b7534c9ac431d8e5da7fc11224b27d20f
Agus <agutierrez@itba.edu.ar>
2024-03-17 18:12:09 -0300

    BFS implementation

--- game_test.py
+++ game_test.py
@@ -39,7 +39,7 @@ MARGIN = 5
 SCREEN_TITLE = "Example Game"
 
 class Sokoban(arcade.Window):
-    def __init__(self, title, grid_data: GridData):
+    def __init__(self, title, grid_data: GridData, showcase=False):
         if len(grid_data.grid) == 0:
             raise ValueError("Initial grid cannot be empty")
         
@@ -57,6 +57,7 @@ class Sokoban(arcade.Window):
         # array is simply a list of lists.
         self.grid_data = grid_data
         self.time_since_last_move = 0
+        self.showcase = showcase
 
         self.explore_data = TreeData([Node(NodeValue(grid_data.player_position, grid_data.boxes_positions, None))], 0, 1)
 
@@ -102,10 +103,102 @@ class Sokoban(arcade.Window):
     def update(self, delta_time):
         if self.paused:
             return
+        
+        # Llamar al algoritmo aca
+        self.time_since_last_move += delta_time
+    
+        if self.time_since_last_move >= 0.01:  # half a second has passed  
+
+            if not self.showcase:
+                step_result = bfs_step(self.grid_data, self.explore_data)
+                new_position = step_result[0]
+                self.grid_data.player_position = new_position.value.player_position
+                self.grid_data.boxes_positions = new_position.value.boxes_positions
+
+                if step_result[1]:
+                    print("Solution found")
+                    print(self.explore_data)
+                    route = []
+                    while new_position.parent:
+                        route.append(new_position.value.direction.name)
+                        new_position = new_position.parent
+                    print(route[::-1])
+                    self.paused = True
+                    return
+                
+                if self.explore_data.queue == []:
+                    print("No solution found")
+                    self.paused = True
+                    return
+            else:
+                # route for base grid.json
+                route = ['LEFT', 'RIGHT', 'RIGHT', 'LEFT', 'UP', 'UP', 'LEFT', 'LEFT', 'DOWN', 'LEFT', 'DOWN', 'DOWN', 'RIGHT', 'UP', 'UP', 'UP', 'UP', 'DOWN', 'DOWN', 'DOWN', 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'UP', 'UP', 'RIGHT', 'UP', 'RIGHT', 'UP', 'RIGHT', 'RIGHT', 'RIGHT', 'LEFT', 'DOWN', 'DOWN', 'RIGHT', 'RIGHT', 'UP', 'UP', 'UP', 'DOWN', 'DOWN', 'DOWN', 'RIGHT', 'RIGHT', 'DOWN', 'RIGHT', 'DOWN', 'DOWN', 'LEFT', 'UP', 'UP', 'UP', 'UP', 'DOWN', 'DOWN', 'DOWN', 'RIGHT', 'RIGHT', 'RIGHT', 'RIGHT', 'UP', 'UP', 'LEFT', 'UP', 'LEFT', 'UP', 'LEFT', 'LEFT', 'LEFT', 'RIGHT', 'DOWN', 'DOWN', 'LEFT', 'LEFT', 'UP', 'UP']
+
+                if self.step < len(route):
+                    result = move_player(Direction[route[self.step]], self.grid_data)
+                    self.grid_data.player_position = result.new_position
+                    self.grid_data.boxes_positions = result.boxes_positions
+                    self.step += 1
+
+            self.time_since_last_move = 0
 
         return super().update(delta_time)
 
+# Check if the player can move into a cell
+def can_move_into_cell(direction, grid_data: GridData, current_position: Coordinate):
+    attempted_position = current_position.move(direction.value)
+    row = attempted_position.row
+    column = attempted_position.column
+    if row < 0 or column < 0 or row >= len(grid_data.grid) or column >= len(grid_data.grid[0]):
+        return False
+    return grid_data.grid[row][column] in {GridElement.EMPTY, GridElement.OBJECTIVE} and grid_data.boxes_positions.count(attempted_position) == 0
 
+# Move the player. Checks if the player can move into a cell and if there is a box in the cell, if the box can be moved
+# Returns a MoveResult with the new position and the new boxes positions, and a boolean indicating if the player moved
+def move_player(direction: Direction, grid_data: GridData): 
+    new_position = grid_data.player_position.move(direction.value)
+    if can_move_into_cell(direction, grid_data, grid_data.player_position):
+        return MoveResult(True, new_position, grid_data.boxes_positions)
+    elif grid_data.boxes_positions.count(new_position) > 0:
+        if can_move_into_cell(direction, grid_data, new_position):
+            box_new_position = new_position.move(direction.value)
+            aux = grid_data.boxes_positions.copy()
+            aux.remove(new_position)
+            aux.append(box_new_position)
+            return MoveResult(True, new_position, aux)
+    return MoveResult(False, grid_data.player_position, grid_data.boxes_positions)
+
+# Explore all possible moves from a given node
+def explore_node(node, grid_data):
+    for direction in Direction:        
+        result = move_player(direction, grid_data)
+        if result.moved:
+            node.add_child(Node(NodeValue(result.new_position, result.boxes_positions, direction)))
+    return node
+    
+# Perform a step in the BFS algorithm
+def bfs_step(grid_data: GridData, data: TreeData):
+    node = data.queue.pop(0)
+    if is_solution(grid_data.grid, node.value.boxes_positions, node.value.player_position):
+        return node, True
+    aux_grid_data = grid_data.copy()
+    aux_grid_data.player_position = node.value.player_position
+    aux_grid_data.boxes_positions = node.value.boxes_positions
+    explore_node(node, aux_grid_data)
+    data.expanded_node_count += 1
+    data.frontier_node_count -= 1
+    for child in node.children:
+        # TODO hash
+        child_state = (child.value.player_position, tuple(sorted(child.value.boxes_positions)))
+        if child_state not in data.visited:  # Check if state has been visited
+            data.queue.append(child)
+            data.frontier_node_count += 1
+            data.visited.add(child_state)  # Add state to visited set
+    return node, False
+    
+# Decide if the game has been solved
+def is_solution(grid, boxes_positions, player_position):
+    return all([grid[coordinate.row][coordinate.column] == GridElement.OBJECTIVE for coordinate in boxes_positions])
 
 
 def main():
